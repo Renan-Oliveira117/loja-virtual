1a07841ce7d8b50f22abec9110558e5b200375c2
Renan Oliveira <renan_oliveira117@hotmail.com>
2020-04-01 16:19:04 -0400

    Carrinho de Compras Finalizado

--- resources/views/publico/inicio.blade.php
+++ resources/views/publico/inicio.blade.php
@@ -5,15 +5,18 @@
 <div class="row mt-2">
         @foreach($produtos as $p)
             <div class="col-md-3">
-                <div class="card mt-2">
-                    <img src="{{asset('/imagens/' . $p->imagem)}}" class="card-img-top" alt="...">
+                <div class="card mt-2">                   
+                    <a href="{{ route('publico.produto.detalhes', $p->id) }}">
+                        <img src="{{asset('/imagens/' . $p->imagem)}}" class="card-img-top" alt="...">
+                    </a>
                         <div class=" card-body">
-                                <h5 class="card-title text-primary">{{ $p->name }}</h5>
+                                <h5 class="card-title text-primary">{{ $p->nome }}</h5>
                                 <h5 class="text-danger">R$ {{ number_format($p->preco, 2, ',', '.') }}</h5>
 
                                 {!! Form::open(['url' => route('publico.carrinho.store')]) !!}
-                                    {!! Form::hidden('produto','$p->id') !!}
-                                    {!! Form::submit('Adicionar ao Carrinho', ['class' => 'btn btn-primary']) !!}
+                                    {!! Form::hidden('produto',$p->id) !!}
+                                    {!! Form::number('quantidade', 1, ['class'=> 'form-control text-center mb-1']) !!}
+                                    {!! Form::submit('Adicionar ao Carrinho', ['class' => 'btn btn-primary btn-block']) !!}
 
                                 {!!Form::close() !!}
                         </div>
